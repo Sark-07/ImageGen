fe9ea9197de47ec35328fadd83f512a943ccc6b3
sark-07 <sarkarpritam0007@gmail.com>
2023-08-14 23:16:04 +0530

    Integrated stable AI's API, Removed OpenAI. Need to Fine tune the best model settings

--- server/controllers/imageGenerationController.js
+++ server/controllers/imageGenerationController.js
@@ -1,24 +1,41 @@
-const {Configuration, OpenAIApi} = require('openai')
+const Replicate = require('replicate')
+// const {Configuration, OpenAIApi} = require('openai')
+
 const imageGenerationController = async (req, res) => {
 
+    const { search } = req.body
+
     try {
-        console.log('hii');
-        const configuration = new Configuration({
-            apiKey: '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',
-          });
-        const openai = new OpenAIApi(configuration);
-        const response = await openai.createImage({
-            prompt: "A cute baby sea otter",
-            n: 2,
-            size: "1024x1024",
-            response_format: 'b64_json'
+        // const configuration = new Configuration({
+        //     apiKey: process.env.OPENAI_API_KEY,
+        //   });
+        // const openai = new OpenAIApi(configuration);
+        // const response = await openai.createImage({
+        //     prompt: search,
+        //     n: 2,
+        //     size: "256x256",
+        //     response_format: 'b64_json'
+        // });
+
+        const replicate = new Replicate({
+            auth: process.env.REPLICATE_API_TOKEN,
         });
 
-        res.status(201).json({success: true, response})
-        
+
+        const input = {
+            prompt: search,
+            num_outputs: 4,
+            scheduler: 'DDIM',
+            refine: 'expert_ensemble_refiner',
+            apply_watermark: true
+        }
+        const output = await replicate.run(process.env.MODEL, { input });
+
+        res.status(201).json({ success: true, response: output })
+
     } catch (error) {
         console.log(error);
-        res.status(500).json({success: false, message: error.message})
+        res.status(500).json({ success: false, message: error.message })
     }
 }
 
